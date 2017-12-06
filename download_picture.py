19b1103f44e12717fb3712570d2a9a4f560d34f4
LIN <hellohelin.huang@gmail.com>
2017-12-06 20:52:36 +0900

    已知图片url，下载图片

---
+++ download_picture.py
@@ -0,0 +1,33 @@
+# -*- coding=utf-8 -*-
+# Python 3.6
+
+import codecs
+import simplejson as json
+import requests
+import time
+
+
+def download(name, img_link):
+    with codecs.open(name, 'wb') as img:
+        img.write(requests.get(img_link).content)
+
+def read_and_download():
+    with codecs.open('games.json', 'r', 'utf-8') as f:
+        game_list = json.load(f)
+        # len(game_list)可以得到长度
+        for id, item in enumerate(game_list, start=1):
+            # name.append(item["title"].replace('/', ' ') + '.jpg')
+            # img_link.append(item["img_link"])
+            name = item["title"].replace('/', ' ') + '.jpg'
+            img_link = item["img_link"]
+            download(name, img_link)
+            print('下载完第 %d 个...' % id)
+
+def main():
+    t0 = time.time()
+    read_and_download()
+    t1 = time.time() - t0
+    print('下载完成！总共使用%f秒' % t1)
+
+if __name__ == '__main__':
+    main()
